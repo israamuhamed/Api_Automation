55b74bc84870739124a500fd0ed29626a892ff07
israamuhamed <59626938+israamuhamed@users.noreply.github.com>
2024-04-24 11:26:32 +0200

    Update EnvironmentSetup.java

--- src/test/java/TestConfig/EnvironmentSetup.java
+++ src/test/java/TestConfig/EnvironmentSetup.java
@@ -5,26 +5,6 @@ import io.restassured.RestAssured;
 public class EnvironmentSetup {
 
     public static String env = "BETA";
-    // env :{ "DEMO", "BETA","LIVE"}
-
-    public static void setEnvironment(String environment) {
-        switch (environment) {
-            case "DEMO":
-                RestAssured.baseURI = "";
-                RestAssured.basePath = "/v1";
-                break;
-            case "BETA":
-                RestAssured.baseURI = "";
-                RestAssured.basePath = "/v1";
-                break;
-            case "LIVE":
-                RestAssured.baseURI = "";
-                RestAssured.basePath = "/v1";
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid environment: " + environment);
-        }
-    }
-
+    // Encironment Setup data  :{ "DEMO", "BETA","LIVE"}
 
 }
